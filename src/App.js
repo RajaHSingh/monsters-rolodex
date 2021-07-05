e555f0874d5846c85f00dba65f90dfda3bcc453d
Raja H singh <RajaHSingh@users.noreply.github.com>
2021-07-05 13:39:43 -0700

    wrap up monsters rolodex

--- src/App.js
+++ src/App.js
@@ -28,6 +28,7 @@ class App extends Component {
     );
     return (
       <div className="App">
+        <h1> React Monster Rolodex</h1>
         <SearchBox
           placeholder="search monsters"
           handleChange={this.handleChange}
